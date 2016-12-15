64d5209f0ae9cbff11d02a5196da3be4d4a733db
Rita Artemov <rartemov@goodwaygroup.com>
2016-12-15 10:44:11 -0800

    fixed the bug

--- sut.spec.js
+++ sut.spec.js
@@ -2,6 +2,6 @@ var output=require('./sut')()
 console.log(output)
 describe('sut', function(){
 it('returns hello rita', function(){
-expect(output).toBe('hello rita')
+expect(output).toBe('Hello Rita')
 })
 })
